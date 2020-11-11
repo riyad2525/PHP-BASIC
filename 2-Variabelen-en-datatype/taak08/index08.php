e700d918e96788b4b0fc77d73f3e9f3bc6e17345
riyad2525 <riyad.mahmud@student.rocva.nl>
2020-11-12 00:07:40 +0100

    var echo list html ul li

---
+++ 2-Variabelen-en-datatype/taak08/index08.php
@@ -0,0 +1,9 @@
+<?php
+
+$voornaam1 = "Riyad";  
+$voornaam2 = "Samy";  
+$voornaam3 = "Fawaz";  
+$voornaam4 = "Mike";  
+  
+echo "<ul><li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li><li>$voornaam4</li></ul>";
+?>
