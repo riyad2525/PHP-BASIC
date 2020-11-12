4463d466e5d80f6393f2176eefb794b4a7dc03d1
riyad2525 <riyad.mahmud@student.rocva.nl>
2020-11-12 21:50:27 +0100

    Array

---
+++ 3-Arrays/taak02/index02.php
@@ -0,0 +1,9 @@
+<?php
+
+$studentName = ['Riyad','Fawaz', 'Samy', 'Mike'];
+$studentName[0] = 'Niels';
+$studentName[] = 'Naim';
+$studentName[] = 'Abdul';
+$studentName[] = 'Samuel';
+print_r($studentName);
+?>
