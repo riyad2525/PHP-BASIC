8ae7fd7af00c55161367d9b92430e87ca390d541
riyad2525 <riyad.mahmud@student.rocva.nl>
2020-11-12 01:04:40 +0100

    later itams list

--- 2-Variabelen-en-datatype/taak10/index10.php
+++ 2-Variabelen-en-datatype/taak10/index10.php
@@ -1,9 +1,8 @@
 <?php
 
-$item1 = "Laptop";
-$item2 .= "tas";
-$item3 .= "boken";
+$item1 = "<li>Laptop</li>";
+$item2 .= "<li>tas</li>";
+$item3 .= "<li>boken</li>";
 
 echo "<ul><li>$item1</li><li>$item2</li><li>$item3</li></ul>";
-
 ?>
